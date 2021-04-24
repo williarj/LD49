b996eea412d85253f521aa24e21ac8c857e3c022
fabbyrob <fabby.j.rob@gmail.com>
2021-04-24 01:08:21 -0400

    basic Gui constructed

---
+++ project/scenes/NotificationScene.gd
@@ -0,0 +1,32 @@
+extends TextureRect
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func startTimer():
+	visible = true
+	$Timer.start()
+
+func _on_Timer_timeout():
+	$Timer.stop()
+	visible = false
+	pass # Replace with function body.
+
+
+func _on_ExitButton_pressed():
+	print("Pressed the button, notification knows.")
+	startTimer()
+	pass # Replace with function body.
---
+++ project/scripts/add_item.gd
@@ -0,0 +1,20 @@
+extends ItemList
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_item("FIRST THING")
+	add_item("SECOND THING")
+	add_item("THIRD THING")
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
